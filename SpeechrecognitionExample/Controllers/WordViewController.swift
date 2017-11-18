22c5dcf80094758c5818cac9212d7211f320c521
Konstantin Golenkov <golenkovkostya@gmail.com>
2017-11-18 19:24:20 +0500

    playing audio

--- SpeechrecognitionExample/Controllers/WordViewController.swift
+++ SpeechrecognitionExample/Controllers/WordViewController.swift
@@ -7,21 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class WordViewController: UIViewController {
     var word = ("","")
     
+    var player:AVAudioPlayer?
+    
+    @IBOutlet weak var transcriptionLabel: UILabel!
+    @IBOutlet weak var wordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        wordLabel.text = word.0
+        transcriptionLabel.text = word.1
+        guard let url = Bundle.main.url(forResource: word.0, withExtension: "mp3") else { return }
+        player = try? AVAudioPlayer(contentsOf: url)
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
-
+    @IBAction func playWordAction(_ sender: Any) {
+        player?.play()
+    }
 }
