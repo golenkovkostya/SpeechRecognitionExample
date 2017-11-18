857d70051e67e130840d11b134eecd0945e3b8f2
Konstantin Golenkov <golenkovkostya@gmail.com>
2017-11-18 19:15:20 +0500

    start template

--- SpeechrecognitionExample/Controllers/WordViewController.swift
+++ SpeechrecognitionExample/Controllers/WordViewController.swift
@@ -1,25 +1,27 @@
 //
-//  ViewController.swift
+//  WordViewController.swift
 //  SpeechrecognitionExample
 //
-//  Created by Kostya Golenkov on 17.11.17.
+//  Created by Kostya Golenkov on 18.11.17.
 //  Copyright © 2017 Kostya Golenkov. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class WordViewController: UIViewController {
+    var word = ("","")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 
 }
-
