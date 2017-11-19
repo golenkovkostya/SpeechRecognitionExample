ba63c15d30045d755bae5eb6ae20a7b3e080bb33
Konstantin Golenkov <golenkovkostya@gmail.com>
2017-11-19 13:08:20 +0500

    speech recognition

--- SpeechrecognitionExample/Controllers/WordViewController.swift
+++ SpeechrecognitionExample/Controllers/WordViewController.swift
@@ -8,22 +8,48 @@
 
 import UIKit
 import AVFoundation
+import Speech
 
 class WordViewController: UIViewController {
     var word = ("","")
     
     var player:AVAudioPlayer?
     
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en_US"))
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    
     @IBOutlet weak var transcriptionLabel: UILabel!
     @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var recognizeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         wordLabel.text = word.0
         transcriptionLabel.text = word.1
+        
+        //configure player
         guard let url = Bundle.main.url(forResource: word.0, withExtension: "mp3") else { return }
         player = try? AVAudioPlayer(contentsOf: url)
-        // Do any additional setup after loading the view.
+        
+        //configure speech recogniton
+        SFSpeechRecognizer.requestAuthorization {[unowned self] (status) in
+            switch status {
+            case .authorized:
+                DispatchQueue.main.async {
+                    [unowned self] in
+                    self.recognizeButton.isEnabled = true
+                }
+            case .denied:
+                print("statu denied")
+            case .notDetermined:
+                print("statu not determined")
+            case .restricted:
+                print("statu restricted")
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -33,5 +59,46 @@ class WordViewController: UIViewController {
     
     @IBAction func playWordAction(_ sender: Any) {
         player?.play()
+    }
+    
+    @IBAction func recognizeSpeechAction(_ sender: UIButton) {
+        if sender.isSelected {
+            audioEngine.stop()
+            request.endAudio()
+            recognitionTask?.cancel()
+        } else {
+            startRecognition()
+        }
+        
+        sender.isSelected = !sender.isSelected
+    }
+    
+    fileprivate func startRecognition () {
+        let node = audioEngine.inputNode
+        let recognitionFormat = node.outputFormat(forBus: 0)
+        
+        node.installTap(onBus: 0, bufferSize: 1024, format: recognitionFormat) {
+            [unowned self](buffer, audioTime) in
+            self.request.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch let error {
+            print("\(error.localizedDescription)")
+            return
+        }
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: {
+            [unowned self] (result, error) in
+            if let res = result?.bestTranscription {
+                DispatchQueue.main.async { [unowned self] in
+                    self.resultLabel.text = res.formattedString;
+                }
+            } else if let error = error {
+                print("\(error.localizedDescription)")
+            }
+        })
     }
 }
